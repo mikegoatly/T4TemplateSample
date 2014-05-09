d13a7859097b66ccb04970ed610fe47764f5f091
Mike Goatly <mike@blinkbox.com>
2014-05-09 10:13:48 +0100

    Copied from zip hosted in blog

---
+++ src/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public DateTime LastLoginDate { get; set; }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer
+            {
+                Name = "Jeffery",
+                LastLoginDate = new DateTime(2009, 11, 2, 19, 23, 32)
+            };
+
+            var template = new CustomerEmailTemplate();
+            template.Session = new Dictionary<string, object>()
+            {
+                { "Customer", customer }
+            };
+
+            template.Initialize();
+            Console.WriteLine(template.TransformText());
+        }
+    }
+}
